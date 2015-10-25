b0bdac094154a587a345ddf22e9bc617fbb4c751
JasonYoucai <liufengnanke@gmail.com>
2015-10-25 20:18:10 +0800

    Add Hour&min2
    Add Hour&min2

--- AwesomeAlarmClock/AwesomeAlarmClock/ViewController.swift
+++ AwesomeAlarmClock/AwesomeAlarmClock/ViewController.swift
@@ -28,11 +28,23 @@ class MyClass {
     }
     
 }
+let MAX_HOUR = 24
+let MAX_MINS = 60
 class ViewController: UIViewController {
+    @IBOutlet weak var myPickerView: UIPickerView!
+    var Hour:[String] = [];
+    var mins:[String] = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        for loop in 0..<MAX_HOUR {
+            Hour.append( String(loop))
+        }
+        for loop in 0..<MAX_MINS {
+            mins.append( String(loop))
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,7 +52,43 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int)->Int {
+        if (component == 0) {
+            return Hour.count
+        }
+        
+        if (component == 1){
+            return mins.count
+        }
+        return 0
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String! {
+        if (component == 0){
+            return Hour[row]
+        }
+        else if (component == 1){
+            return mins[row]
+        }
+        
+        return ""
+
+    }
+    
+    
+    
     @IBAction func startGame(){
+        let firstNum = myPickerView.selectedRowInComponent(0)
+        let secondNum = myPickerView.selectedRowInComponent(1)
+        var selectinfo : String = "\nSelect Info is [\(Hour[firstNum])][\(mins[secondNum])] "
+            //println("province:(provinces.keys.array[provinceNum!]);city:(cities[cityNum!])")
+        
+//
         let myalert = UIAlertView()
         var total = 0;
         
@@ -63,6 +111,9 @@ class ViewController: UIViewController {
             allInfo += xx
         }
         
+        var myclass  = MyClass(name : "Baby", age : 100)
+        
+        myalert.title = "准备好了吗\(total)\(allInfo)\(myclass.totalName)\(selectinfo)"
         myalert.message = "准备好开始了吗？"
         myalert.addButtonWithTitle("Ready, go!")
         myalert.show()
